3fbd6da5acb5c21b5755021eae133eae342c5048
Ivan <ivanristichofren@gmail.com>
2026-09-17 19:04:15 +0200

    pretty update

--- Resurang.py
+++ Resurang.py
@@ -121,12 +121,13 @@ if "nej" in sittingDown:
 """ En sammanfattning av programmet är att det är en enkel kassörsprogram som tar emot beställningar 
     för pizza och dryck, beräknar totalpriset inklusive eventuella tillägg och dricks, och ger 
     användaren möjlighet att sitta ner och vänta på sin beställning. 
-    Programmet använder sig av if-satser för att hantera olika val och inmatningar från användaren. """
+    Programmet använder sig av if-satser för att hantera olika val och inmatningar från användaren.
 
 
-# int = 3
-# float = 3.0
-# str = "3"
-# You can get the type of a varible by using type() ex= print(type(x))
-# A and a is different varibles, it's case sensitive
 
+int = 3
+float = 3.0
+str = "3"
+You can get the type of a varible by using type() ex= print(type(x))
+A and a is different varibles, it's case sensitive
+"""
